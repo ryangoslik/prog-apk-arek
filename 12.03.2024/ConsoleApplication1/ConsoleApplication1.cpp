bf39bc4118e515bb4f42fd07e8587e2ae0de9d8c
pawelce <szkolacecot@gmail.com>
2024-03-12 10:29:58 +0100

    123

---
+++ 12.03.2024/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class Pojazdy {
+private:
+    string name;
+public:
+    Pojazdy(string n) {
+        this->name = n;
+    }
+    void prezentacjaPojazdu() {
+        cout << "Prezentacja pojazdu: " << this->name << endl;
+    }
+};
+
+class Samochody : public Pojazdy{
+private:
+    string marka;
+    string nazwa;
+public:
+    Samochody(string m, string n):Pojazdy(n) {
+        this->marka = m;
+        this->nazwa = n;
+    }
+    void prezentacjaSamochodu() {
+        cout << "Prezentacja samochodu: " << this->marka << endl;
+        cout << "Nazwa pojazdu: " << this-> nazwa << endl;
+    }
+};
+
+int main()
+{
+    Samochody vw = Samochody("Volkswagen", "Passat 1.9TDI");
+    vw.prezentacjaPojazdu();
+    vw.prezentacjaSamochodu();
+    return EXIT_SUCCESS;
+}
