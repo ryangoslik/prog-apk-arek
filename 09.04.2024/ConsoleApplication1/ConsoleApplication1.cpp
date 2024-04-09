35a4786f38c1be3a8e1d09f401c6e003f2d203c0
pawelce <szkolacecot@gmail.com>
2024-04-09 10:24:01 +0200

    123

---
+++ 09.04.2024/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,63 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Ksztalt{
+public:
+    virtual void oblicz() = 0;
+};
+
+class Trojkat:public Ksztalt {
+public:
+    virtual void oblicz() {
+        cout << "obliczam trojkat" << endl;
+    };
+};
+
+class Prostokat:public Ksztalt {
+public:
+    virtual void oblicz() {
+        cout << "obliczam Prostokat" << endl;
+    };
+};
+
+class Kwadrat:public Ksztalt {
+public:
+    virtual void oblicz() {
+        cout << "obliczam Kwadrat" << endl;
+    };
+};
+
+class Kolo:public Ksztalt{
+public:
+    virtual void oblicz() {
+        cout << "obliczam Kolo" << endl;
+    };
+};
+
+int main()
+{
+    Ksztalt* wsk;
+    Trojkat tr;
+    Prostokat pr;
+    Kwadrat kw;
+    Kolo kol;
+
+    cout << "Teraz Trojkat chopie..." << endl;
+    wsk = &tr;
+    wsk->oblicz();
+
+    cout << "Teraz Prostokat chopie..." << endl;
+    wsk = &pr;
+    wsk->oblicz();
+
+    cout << "Teraz kwadrat chopie..." << endl;
+    wsk = &kw;
+    wsk->oblicz();
+
+    cout << "Teraz kolo chopie..." << endl;
+    wsk = &kol;
+    wsk->oblicz();
+    return EXIT_SUCCESS;
+}
+
---
+++ 09.04.2024/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,63 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Postac {
+public:
+    virtual void prezentacjaPostaci() = 0;
+};
+
+class DroidB1 :public Postac {
+public:
+    virtual void prezentacjaPostaci() {
+        cout << "Separatysci, podstawowa jednostka" << endl;
+    };
+};
+
+class DroidB2 :public Postac {
+public:
+    virtual void prezentacjaPostaci() {
+        cout << "Separatysci, pancerz, ciezkie wyposazenie" << endl;
+    };
+};
+
+class Droideka :public Postac {
+public:
+    virtual void prezentacjaPostaci() {
+        cout << "Separatysci specialisci, ciezkie wyposazenie" << endl;
+    };
+};
+
+class DroidCommando :public Postac {
+public:
+    virtual void prezentacjaPostaci() {
+        cout << "Separatysci, specialne jednostki" << endl;
+    };
+};
+
+void krainaPostaci(Postac& ob) {
+    ob.prezentacjaPostaci();
+}
+
+int main()
+{
+
+    DroidB1 b;
+    DroidB2 a;
+    Droideka dr;
+    DroidCommando bx;
+
+    cout << "Droid bojowy B1" << endl;
+    krainaPostaci(b);
+
+    cout << "Droid bojowy B2" << endl;
+    krainaPostaci(a);
+
+    cout << "Droid bojowy droideka" << endl;
+    krainaPostaci(dr);
+
+    cout << "Droid bojowy BX" << endl;
+    krainaPostaci(bx);
+    return EXIT_SUCCESS;
+}
+
