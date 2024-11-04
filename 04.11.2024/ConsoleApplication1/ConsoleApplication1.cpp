59b9783fd90a807c2ead1d62099d61086126322a
Jezyk <kubusjezyk69@gmail.com>
2024-11-04 07:42:37 +0100

    123

---
+++ 04.11.2024/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Silnik {
+public:
+    int moc;
+    int pojemnosc;
+    Silnik() : moc(0), pojemnosc(0) {};
+    Silnik(const int& m, const int& p) {
+        this->moc = m;
+        this->pojemnosc = p;
+    }
+    void Wyswietl() {
+        cout << "Moc: " << moc << "  Pojemnosc: " << pojemnosc << endl;
+    }
+};
+
+class Samochod {
+public:
+    string marka;
+    string model;
+    Silnik silnik;
+    Samochod(const string& ma, const string& mo, const Silnik& s) {
+        this->marka = ma;
+        this->model = mo;
+        this->silnik = s;
+    }
+    void Wyswietl() {
+        cout << "Marka: " << marka << "  model: " << model << endl;
+    }
+};
+
+
+int main()
+{
+    Silnik silnik(155, 1999);
+    Samochod samochod("Audi", "A5", silnik);
+    silnik.Wyswietl();
+    samochod.Wyswietl();
+}
+
---
+++ 04.11.2024/kartkowka/kartkowka.cpp
@@ -0,0 +1,45 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Chapter{
+public:
+    string tytul;
+    int l_stron;
+    Chapter() : tytul(""), l_stron(0) {};
+    Chapter(const string& t, const int& l) {
+        this->tytul = t;
+        this->l_stron = l;
+    }
+    void Wyswietl() {
+        cout << "Tytul rozdzialu: " << tytul << "  liczba stron: " << l_stron << endl;
+    }
+};
+
+class Book {
+public:
+    string tytul;
+    string autor;
+    int l_rozdzialow;
+    Chapter chapter;
+    Book(const string& t, const string& a, const int l_roz, Chapter& c) {
+        this->tytul = t;
+        this->autor = a;
+        this->l_rozdzialow = l_roz;
+        this->chapter = c;
+    }
+    void Wyswietl() {
+        cout << "Tytul ksiazki: " << tytul << "  Autor: " << autor << "  liczba rozdzialow: " << l_rozdzialow << endl;
+    }
+};
+
+int main()
+{
+    Chapter chapter("Rozdzial1", 100);
+    Book book("Fajna ksiazka", "Fajny Autor", 10, chapter);
+
+    chapter.Wyswietl();
+    book.Wyswietl();
+}
+
